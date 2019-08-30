4c9eb6997d6ff85fa30bcbc62934c61bdff09d2a
Tanger <2437889166@qq.com>
2019-08-30 16:06:14 +0800

    Update 旋转数组.cpp

--- 旋转数组.cpp
+++ 旋转数组.cpp
@@ -43,6 +43,7 @@ public:
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
         reverse(nums,0,nums.size()-1);
         reverse(nums,0,k-1);
         reverse(nums,k,nums.size()-1);
