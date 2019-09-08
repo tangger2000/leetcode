d8312fd13811bef4935193c4de63d61a7d921849
Tanger <2437889166@qq.com>
2019-09-09 00:45:10 +0800

    Create 移动零.cpp

---
+++ 移动零.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==0)
+                continue;
+            
+            int j=i;
+            while (j>0&&nums[j-1]==0&&nums[j]!=0)
+            {
+                nums[j-1]=nums[j];
+                nums[j]=0;
+                j--;
+            }
+        }
+    }
+};
